f868c15942af20a2fb06df39574f5f1cebeb44a2
ELKAst-1 <sementrepp@icloud.com>
2025-11-23 13:18:24 +0700

    Delete PythonProject directory

--- PythonProject/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher
-from .common import register_common_handlers
-from .user import register_user_handlers
-from .admin import register_admin_handlers
-
-def register_all_handlers(dp: Dispatcher, bot):
-    register_common_handlers(dp)
-    register_user_handlers(dp)
-    register_admin_handlers(dp)
